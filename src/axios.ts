223f13c368a57505cfbeca86c08e65d5c0cfdac3
Lazy Bear <c0mzero314159265@gmail.com>
2022-11-25 08:48:17 +0700

    add backend ui

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-axios.defaults.baseURL =
-    process.env.NEXTBACKED_URL || 'http://localhost:8000/v1'
+axios.defaults.baseURL = 'http://178.128.26.253:8000/v1'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 export default axios
